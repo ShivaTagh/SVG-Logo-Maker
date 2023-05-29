9c4d16fe2854fa7133e8f6e3ad7989e27b0f81c9
ShivaTagh <127795324+ShivaTagh@users.noreply.github.com>
2023-05-28 23:36:57 -0700

    finished the codes

---
+++ index.js
@@ -0,0 +1,45 @@
+// Packages
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateSVG = require('./lib/shapes').generateSVG;
+
+// Inquirer questions
+const questions = [
+    {
+        message: 'Enter text for your logo.',
+        name: 'text',
+        validate: async (input) => {
+            return (input.length > 3 ? 'Logo text must be 3 characters or less' : true);
+        }
+    },
+    {
+        message: 'Enter a text color for your logo.',
+        name: 'textColor'
+    },
+    {
+        type: 'list',
+        message: 'Choose a shape for your logo.',
+        name: 'shape',
+        choices: [
+            'circle',
+            'triangle',
+            'square'
+        ]
+    },
+    {
+        message: 'Enter a shape color for your logo.',
+        name: 'shapeColor'
+    },
+];
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => err ? console.error(err) : console.log(`Wrote data to ${fileName}`));
+}
+
+function init() {
+    inquirer
+    .prompt(questions)
+    .then((response) => writeToFile('./Assets/circle.svg', generateSVG(response)));
+}
+
+init();
